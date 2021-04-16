4134e2158991314bfe78bf1e7b665ae24a422943
aoyuzhan <aoyuzhan@amazon.com>
2021-04-16 08:19:58 +0000

    inverted-list to be test

--- src/offline/inverted-list/inverted-list.py
+++ src/offline/inverted-list/inverted-list.py
@@ -1,14 +1,26 @@
-# importing libraries
-import argparse
+from __future__ import print_function
 import os
+import sys
+import math
 import pickle
-
 import boto3
-import faiss
+import os
+import numpy as np
+import kg
+import encoding
 import pandas as pd
-from tqdm import tqdm
+# from tqdm import tqdm
+import time
+import argparse
+import logging
+import re
 
-import service_impl
+# tqdm.pandas()
+# pandarallel.initialize(progress_bar=True)
+# bucket = os.environ.get("BUCKET_NAME", " ")
+# raw_data_folder = os.environ.get("RAW_DATA", " ")
+
+s3client = boto3.client('s3')
 
 ########################################
 # 从s3同步数据
@@ -35,137 +47,210 @@ def write_to_s3(filename, bucket, key):
             Body=f
         )
 
-
 def write_str_to_s3(content, bucket, key):
     print("write s3://{}/{}, content={}".format(bucket, key, content))
     s3client.put_object(Body=str(content).encode("utf8"), Bucket=bucket, Key=key, ACL='bucket-owner-full-control')
 
-
-default_bucket = 'sagemaker-us-east-1-002224604296'
-default_mk_region = '1'
-default_level_1 = 'recommender-system-film-mk'
+default_bucket = 'aws-gcr-rs-sol-workshop-ap-southeast-1-522244679887'
+default_prefix = 'sample-data'
 parser = argparse.ArgumentParser()
 parser.add_argument('--bucket', type=str, default=default_bucket)
-parser.add_argument('--mk-region', type=str, default=default_mk_region)
-parser.add_argument('--level-one', type=str, default=default_level_1)
+parser.add_argument('--prefix', type=str, default=default_prefix)
 args, _ = parser.parse_known_args()
 bucket = args.bucket
-mk_region = args.mk_region
-level_1 = args.level_one
-
-prefix = f"{level_1}/{mk_region}"
+prefix = args.prefix
 
 print("bucket={}".format(bucket))
 print("prefix='{}'".format(prefix))
 
+out_s3_path = "s3://{}/{}/feature/content/inverted-list".format(bucket, prefix)
+
 local_folder = 'info'
 if not os.path.exists(local_folder):
     os.makedirs(local_folder)
-# 行为数据加载
+# 行为/物品数据同步
 file_name_list = ['action.csv']
-s3_folder = '{}/system/user-data/clean/latest'.format(prefix)
+s3_folder = '{}/system/action-data'.format(prefix)
 sync_s3(file_name_list, s3_folder, local_folder)
-# ub数据加载
-file_name_list = ['ub_item_vector.index', 'embed_raw_item_mapping.pickle']
-s3_folder = '{}/feature/action/'.format(prefix)
-sync_s3(file_name_list, s3_folder, local_folder)
-# 用户画像数据加载
-file_name_list = ['portrait.pickle']
-s3_folder = '{}/feature/recommend-list/portrait'.format(prefix)
-sync_s3(file_name_list, s3_folder, local_folder)
-# 倒排列表的pickle文件
-file_name_list = ['movie_id_movie_property_dict.pickle',
-                  'movie_category_movie_ids_dict.pickle',
-                  'movie_director_movie_ids_dict.pickle',
-                  'movie_actor_movie_ids_dict.pickle',
-                  'movie_language_movie_ids_dict.pickle',
-                  'movie_level_movie_ids_dict.pickle',
-                  'movie_year_movie_ids_dict.pickle']
-s3_folder = '{}/feature/content/inverted-list/'.format(prefix)
+file_name_list = ['item.csv']
+s3_folder = '{}/system/item-data'.format(prefix)
 sync_s3(file_name_list, s3_folder, local_folder)
 
-file_name_list = ['recall_config.pickle']
-s3_folder = '{}/model/recall'.format(prefix)
-sync_s3(file_name_list, s3_folder, local_folder)
+df_filter_item = pd.read_csv('info/item.csv',sep='_!_',names=['news_id','type_code','type','title','keywords','popularity','new'])
 
-# 加载pickle文件
-file_to_load = open("info/movie_id_movie_property_dict.pickle", "rb")
-dict_id_content = pickle.load(file_to_load)
-print("length of movie_id v.s. movie_property {}".format(len(dict_id_content)))
+df_filter_action = pd.read_csv('info/action.csv',sep='_!_',names=['user_id','news_id','timestamp','action_type','action'])
 
-file_to_load = open("info/movie_category_movie_ids_dict.pickle", "rb")
-dict_category_id = pickle.load(file_to_load)
-print("length of movie_category v.s. movie_ids {}".format(len(dict_category_id)))
+df_item_stats = df_filter_action[['news_id','action_type','action']]
+df_item_stats = df_item_stats.groupby(['news_id','action_type']).sum()
+df_item_stats = df_item_stats.reset_index()
+df_item_stats['action'] = df_item_stats['action'] / df_item_stats['action'].abs().max() * 10.0
 
-file_to_load = open("info/movie_director_movie_ids_dict.pickle", "rb")
-dict_director_id = pickle.load(file_to_load)
-print("length of movie_dicrector v.s. movie_ids {}".format(len(dict_director_id)))
+pd_merge_result = pd.merge(df_filter_item, df_item_stats, on="news_id", how="left").drop(columns=['action_type'])
+pd_merge_result = pd_merge_result.fillna(0)
 
-file_to_load = open("info/movie_actor_movie_ids_dict.pickle", "rb")
-dict_actor_id = pickle.load(file_to_load)
-print("length of movie_actor v.s. movie_ids {}".format(len(dict_actor_id)))
+# prepare model for batch process
+os.environ['GRAPH_BUCKET'] = 'sagemaker-us-east-1-002224604296'
+os.environ['KG_DBPEDIA_KEY'] = 'recommender-system-data/model/sort/content/words/mapping/kg_dbpedia.txt'
+os.environ['KG_ENTITY_KEY'] = 'recommender-system-data/model/sort/content/words/mapping/entities_dbpedia.dict'
+os.environ['KG_RELATION_KEY'] = 'recommender-system-data/model/sort/content/words/mapping/relations_dbpedia.dict'
+os.environ['KG_ENTITY_INDUSTRY_KEY'] = 'recommender-system-data/model/sort/content/words/mapping/entity_industry.txt'
+os.environ['KG_VOCAB_KEY'] = 'recommender-system-data/model/sort/content/words/mapping/vocab.json'
+os.environ['DATA_INPUT_KEY'] = ''
+os.environ['TRAIN_OUTPUT_KEY'] = 'recommender-system-data/model/sort/content/kg/news/gw/'
+kg_path = os.environ['GRAPH_BUCKET']
+dbpedia_key = os.environ['KG_DBPEDIA_KEY']
+entity_key = os.environ['KG_ENTITY_KEY']
+relation_key = os.environ['KG_RELATION_KEY']
+entity_industry_key = os.environ['KG_ENTITY_INDUSTRY_KEY']
+vocab_key = os.environ['KG_VOCAB_KEY']
+data_input_key = os.environ['DATA_INPUT_KEY']
+train_output_key = os.environ['TRAIN_OUTPUT_KEY']
 
-file_to_load = open("info/movie_language_movie_ids_dict.pickle", "rb")
-dict_language_id = pickle.load(file_to_load)
-print("length of movie_lanugage v.s. movie_ids {}".format(len(dict_language_id)))
+env = {
+    'GRAPH_BUCKET': kg_path,
+    'KG_DBPEDIA_KEY': dbpedia_key,
+    'KG_ENTITY_KEY': entity_key,
+    'KG_RELATION_KEY': relation_key,
+    'KG_ENTITY_INDUSTRY_KEY': entity_industry_key,
+    'KG_VOCAB_KEY': vocab_key,
+    'DATA_INPUT_KEY': data_input_key,
+    'TRAIN_OUTPUT_KEY': train_output_key
+}
+graph = kg.Kg(env)  # Where we keep the model when it's loaded
+model = encoding.encoding(graph, env)
 
-file_to_load = open("info/movie_level_movie_ids_dict.pickle", "rb")
-dict_level_id = pickle.load(file_to_load)
-print("length of movie_level v.s. movie_ids {}".format(len(dict_level_id)))
+# generate dict_id_keywords for tfidf
+dict_keywords_id = {}
+for row in df_filter_item.iterrows():
+    item_row = row[1]
+    program_id = str(item_row['news_id'])
+    for kw in item_row['keywords'].split(','):
+        if kw not in dict_keywords_id.keys():
+            dict_keywords_id[kw] = [program_id]
+            continue
+        current_list = dict_keywords_id[kw]
+        current_list.append(program_id)
+        dict_keywords_id[kw].append(program_id)
+n_keyword_whole = len(dict_keywords_id)
 
-file_to_load = open("info/movie_year_movie_ids_dict.pickle", "rb")
-dict_year_id = pickle.load(file_to_load)
-print("length of movie_year v.s. movie_ids {}".format(len(dict_year_id)))
+def get_tfidf(category_property):
+    if not category_property or str(category_property).lower() in ['nan', 'nr', '']:
+        return [None]
+    if not category_property:
+        return [None]
+    value = [item.strip() for item in category_property.split(',')]
+    keywords_tfidf = {}
+    for keyword in value:
+        current_score = 1 / len(value)*math.log(n_keyword_whole / len(dict_keywords_id[keyword]))
+        keywords_tfidf[keyword] = current_score
+    return keywords_tfidf
+        
+def get_category(category_property):
+    if not category_property or str(category_property).lower() in ['nan', 'nr', '']:
+        return [None]
+    if not category_property:
+        return [None]
+    return [item.strip().lower() for item in category_property.split(',')]
+            
+def get_single_item(item):
+    if not item or str(item).lower().strip() in ['nan', 'nr', '']:
+        return [None]
+    return [str(item).lower().strip()]
 
-file_to_load = open("info/recall_config.pickle", "rb")
-recall_config = pickle.load(file_to_load)
-print("config recall")
+def get_entities(title):
+    return model[title]
 
-ub_faiss_index = faiss.read_index('info/ub_item_vector.index')
+def single_dict(raw_dict, feat, item_id):
+    if feat not in raw_dict.keys():
+        raw_dict[feat] = [item_id]
+    else:
+        current_list = raw_dict[feat]
+        current_list.append(item_id)
+        raw_dict[feat] = current_list
 
-file_to_load = open("info/embed_raw_item_mapping.pickle", "rb")
-ub_idx_mapping = pickle.load(file_to_load)
-print("length of item mapping {}".format(len(ub_idx_mapping)))
+def list_dict(raw_dict, feat_list, item_id):
+    for feat in feat_list:
+        single_dict(raw_dict, feat, item_id)
 
-file_to_load = open("info/portrait.pickle", "rb")
-user_portrait = pickle.load(file_to_load)
-print("length of user_portrait {}".format(len(user_portrait)))
+def update_popularity(item_df, action_df):
+    pd_merge_result = pd.merge(item_df, action_df, on="news_id", how="left").drop(columns=['action_type'])
+    pd_merge_result = pd_merge_result.fillna(0)
+    df_update = pd_merge_result.drop(columns=['popularity']).rename(columns={"action":"popularity"})
+    return df_update
+        
+def sort_by_score(df):
+    logging.info("sort_by_score() enter, df.columns: {}".format(df.columns))
+    df['popularity'].fillna(0, inplace=True)
 
-# 配置参数
-config_dict = {}
-recall_wrap = {}
-recall_wrap['content'] = dict_id_content
-recall_wrap['dict_wrap'] = {}
-recall_wrap['dict_wrap']['category'] = dict_category_id
-recall_wrap['dict_wrap']['director'] = dict_director_id
-recall_wrap['dict_wrap']['actor'] = dict_actor_id
-recall_wrap['dict_wrap']['language'] = dict_language_id
-recall_wrap['dict_wrap']['level'] = dict_level_id
-recall_wrap['dict_wrap']['year'] = dict_year_id
-recall_wrap['config'] = recall_config
-config_dict['recall_wrap'] = recall_wrap
-recall_wrap['ub_index'] = ub_faiss_index
-recall_wrap['ub_idx_mapping'] = ub_idx_mapping
-# 加载所有人的数据
-action_data_pddf = pd.read_csv("info/action.csv", sep='\t')
-print("load {} action data".format(len(action_data_pddf)))
-# 初始化recall结果
-recall_batch_result = {}
-# print(config_dict)
-recall_batch_function = service_impl.ServiceImpl()
-for reviewerID, hist in tqdm(
-        action_data_pddf[(action_data_pddf['label'] == 1) & (action_data_pddf['programType'] == 1)].groupby('userid')):
-    pos_list = hist['programId'].tolist()
-    config_dict['user_portrait'] = user_portrait[str(reviewerID)]
-    # user_click_records[reviewerID] = pos_list
-    recall_batch_result[str(reviewerID)] = recall_batch_function.merge_recall_result([str(elem) for elem in pos_list],
-                                                                                     **config_dict)
-# 存储recall的结果
-file_name = "info/recall_batch_result.pickle"
-out_file = open(file_name, 'wb')
-pickle.dump(recall_batch_result, out_file)
-out_file.close()
+    df['popularity_log'] = np.log1p(df['popularity'])
+    popularity_log_max = df['popularity_log'].max()
+    popularity_log_min = df['popularity_log'].min()
 
-write_to_s3(file_name,
-            bucket,
-            '{}/feature/recommend-list/movie/recall_batch_result.pickle'.format(prefix))
+    df['popularity_scaled'] = ((df['popularity_log'] - popularity_log_min) / (
+            popularity_log_max - popularity_log_min)) * 10
+
+    df_sorted = df.sort_values(by='popularity_scaled', ascending=False)
+    
+    df_sorted = df_sorted.drop(
+        ['popularity_log', 'popularity_scaled'], axis=1)
+
+    logging.info("sort_by_score() return, df.columns: {}".format(df_sorted.columns))
+    return df_sorted
+
+def get_bucket_key_from_s3_path(s3_path):
+    m = re.match(r"s3://(.*?)/(.*)", s3_path)
+    return m.group(1), m.group(2)
+
+def gen_pickle_files(df, action_df):
+    df_update = update_popularity(df, action_df)
+    df_sort = sort_by_score(df_update)
+    
+    news_id_news_property_dict = {}
+    news_type_news_ids_dict = {}
+    news_keywords_news_ids_dict = {}
+    news_entities_news_ids_dict = {}
+    news_words_news_ids_dict = {}
+    
+    for row in df_sort.iterrows():
+        item_row = row[1]
+        program_id = str(item_row['news_id'])
+        current_entities = get_entities(item_row['title'])[0]
+        current_words = get_entities(item_row['title'])[1]
+        program_dict = {
+            'title': get_single_item(item_row['title']),
+            'type': get_single_item(item_row['type']),
+            'keywords': get_category(item_row['keywords']),
+            'tfidf': get_tfidf(item_row['keywords']),
+            'entities': current_entities,
+            'words': current_words
+        }
+        news_id_news_property_dict[program_id] = program_dict
+        list_dict(news_type_news_ids_dict, program_dict['type'], program_id)
+        list_dict(news_keywords_news_ids_dict, program_dict['keywords'], program_id)
+        list_dict(news_entities_news_ids_dict, program_dict['entities'], program_id)
+        list_dict(news_words_news_ids_dict, program_dict['words'], program_id)
+
+    result_dict = {
+        'news_id_news_property_dict': news_id_news_property_dict,
+        'news_type_news_ids_dict': news_type_news_ids_dict,
+        'news_keywords_news_ids_dict': news_keywords_news_ids_dict,
+        'news_entities_news_ids_dict': news_entities_news_ids_dict,
+        'news_words_news_ids_dict': news_words_news_ids_dict
+    }
+    return result_dict
+
+rd = gen_pickle_files(df_filter_item, df_item_stats)
+
+bucket, out_prefix = get_bucket_key_from_s3_path(out_s3_path)
+for dict_name, dict_val in rd.items():
+    file_name = f'{dict_name}.pickle'
+    # print("pickle =>", file_name)
+    out_file = open(file_name, 'wb')
+    pickle.dump(dict_val, out_file)
+    out_file.close()
+    # s3_url = S3Uploader.upload(file_name, out_s3_path)
+    s3_url = write_to_s3(file_name, bucket, f'{out_prefix}/{file_name}')
+    logging.info("write {}".format(s3_url))
+
+
+
