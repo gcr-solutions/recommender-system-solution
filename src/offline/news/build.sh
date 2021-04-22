eeea3f818851eaa34e2d2fe88cb661d765b9db54
amliuyong <23968560@qq.com>
2021-04-22 10:55:31 +0800

    add more steps

--- src/offline/news/build.sh
+++ src/offline/news/build.sh
@@ -4,16 +4,18 @@ export REGION='ap-southeast-1'
 
 steps=(
 action-preprocessing
-dashboard
 inverted-list
-model-update-graph
-recall-batch
-add-item-user-batch
-item-feature-update-batch
-portrait-batch
-filter-batch
 item-preprocsssing
+model-update-embedding
 rank-batch
+step_funcs
+add-item-user-batch
+dashboard
+filter-batch
+item-feature-update-batch
+model-update-action
+portrait-batch
+recall-batch
 weight-update-batch
 )
 build_dir=$(pwd)
@@ -30,5 +32,5 @@ echo ">> Build lambda ..."
 
 cd ${build_dir}/step_funcs
 echo ">> Build step_funcs ..."
-# ./build.sh
+ ./build.sh
 
