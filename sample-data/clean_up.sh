66ba4c62c85097acdb790c8b605d32b6292f6fc1
amliuyong <23968560@qq.com>
2021-04-29 16:54:23 +0800

    add clean_up

---
+++ sample-data/clean_up.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+echo "run $0 ..."
+pwd
+
+if [[ -z $REGION ]];then
+    REGION='ap-northeast-1'
+fi
+
+echo "REGION: $REGION"
+
+AWS_ACCOUNT_ID=$(aws  sts get-caller-identity  --o text | awk '{print $1}')
+echo "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID}"
+
+BUCKET=aws-gcr-rs-sol-workshop-${REGION}-${AWS_ACCOUNT_ID}
+S3Prefix=sample-data
+
+aws  s3 rm s3://${BUCKET}/${S3Prefix}  --recursive
+
+
