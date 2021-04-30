600340759eb681f52af4bdad036e6cac0fa0496a
amliuyong <23968560@qq.com>
2021-04-30 11:05:11 +0800

    new item

---
+++ sample-data/new_item_to_s3.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo "################"
+echo "run $0 ..."
+pwd
+
+if [[ -z $REGION ]];then
+    REGION='ap-northeast-1'
+fi
+
+echo "REGION: $REGION"
+
+AWS_ACCOUNT_ID=$(aws  sts get-caller-identity  --o text | awk '{print $1}')
+echo "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID}"
+
+BUCKET_BUILD=aws-gcr-rs-sol-workshop-${REGION}-${AWS_ACCOUNT_ID}
+PREFIX=sample-data
+
+echo "BUCKET_BUILD=${BUCKET_BUILD}"
+echo "Create S3 Bucket: ${BUCKET_BUILD} if not exist"
+aws  s3 mb s3://${BUCKET_BUILD}  >/dev/null 2>&1
+
+echo "########################################################"
+echo "aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/"
+echo "########################################################"
+
+cp item_new.csv ./ingest-data/item/
+
+aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/
+if [[ $? -ne 0 ]]; then
+      echo "error!!! aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/"
+      exit 1
+fi
+
