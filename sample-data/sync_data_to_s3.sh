2630fcc5059ad0fd8065380c03df0c8925edbf5c
amliuyong <23968560@qq.com>
2021-04-30 13:35:16 +0800

    color log

--- sample-data/sync_data_to_s3.sh
+++ sample-data/sync_data_to_s3.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
 echo "################"
+Red=$'\e[1;31m'
+Green=$'\e[1;32m'
+Yellow=$'\e[1;33m'
+Blue=$'\e[1;34m'
+
+OK_print () {
+   echo -e "$Green $1 \e[39m"
+}
+Error_print() {
+	 echo -e "$'\e[1;31m' $1 \e[39m"
+}
+
+Yellow_print() {
+  echo -e "$Yellow $1 \e[39m"
+}
+
+Blue_print() {
+  echo -e "$Blue $1 \e[39m"
+}
+
 echo "run $0 ..."
 pwd
 
@@ -11,7 +31,7 @@ fi
 echo "REGION: $REGION"
 
 AWS_ACCOUNT_ID=$(aws  sts get-caller-identity  --o text | awk '{print $1}')
-echo "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID}"
+Yellow_print "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID}"
 
 BUCKET_BUILD=aws-gcr-rs-sol-workshop-${REGION}-${AWS_ACCOUNT_ID}
 PREFIX=sample-data
@@ -21,12 +41,13 @@ echo "Create S3 Bucket: ${BUCKET_BUILD} if not exist"
 aws  s3 mb s3://${BUCKET_BUILD}  >/dev/null 2>&1
 
 echo "########################################################"
-echo "aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/"
+Blue_print "aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/"
 echo "########################################################"
 
 aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/
 if [[ $? -ne 0 ]]; then
-      echo "error!!! aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/"
+      Error_print "error!!! aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/"
       exit 1
 fi
 
+OK_print "data sync completed"
