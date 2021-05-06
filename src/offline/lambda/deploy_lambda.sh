d381fbbc35898550de8cf48e2b6f59c980df0d5f
amliuyong <23968560@qq.com>
2021-05-06 10:20:41 +0800

    add UPDATE_COMPLETE

--- src/offline/lambda/deploy_lambda.sh
+++ src/offline/lambda/deploy_lambda.sh
@@ -62,7 +62,7 @@ aws  cloudformation deploy --region ${REGION} \
 --capabilities CAPABILITY_NAMED_IAM
 
  StackStatus=$(aws  cloudformation  describe-stacks --region ${REGION} --stack-name ${STACK_NAME} --output table | grep StackStatus)
- echo ${StackStatus} |  grep CREATE_COMPLETE > /dev/null
+ echo ${StackStatus} |  egrep "(CREATE_COMPLETE)|(UPDATE_COMPLETE)" > /dev/null
 
  if [[ $? -ne 0 ]]; then
       Error_print "error!!!  ${StackStatus}"
--- src/offline/step_funcs/build.sh
+++ src/offline/step_funcs/build.sh
@@ -64,7 +64,7 @@ do
     --capabilities CAPABILITY_NAMED_IAM
 
      StackStatus=$(aws  cloudformation  describe-stacks --region ${REGION} --stack-name ${STACK_NAME} --output table | grep StackStatus)
-     echo ${StackStatus} |  grep CREATE_COMPLETE > /dev/null
+     echo ${StackStatus} |  egrep "(CREATE_COMPLETE)|(UPDATE_COMPLETE)" > /dev/null
 
      if [[ $? -ne 0 ]]; then
          Error_print "error!!!  ${StackStatus}"
