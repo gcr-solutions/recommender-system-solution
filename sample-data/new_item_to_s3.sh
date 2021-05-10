32bff17e6e987f0464bac8a0afd613d92a11448d
liuyong <23968560@qq.com>
2021-05-10 15:55:03 +0800

    change sync to cp

--- sample-data/new_item_to_s3.sh
+++ sample-data/new_item_to_s3.sh
@@ -13,24 +13,17 @@ echo "REGION: $REGION"
 AWS_ACCOUNT_ID=$(aws  sts get-caller-identity  --o text | awk '{print $1}')
 echo "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID}"
 
-BUCKET_BUILD=aws-gcr-rs-sol-workshop-${REGION}-${AWS_ACCOUNT_ID}
+BUCKET=aws-gcr-rs-sol-workshop-${REGION}-${AWS_ACCOUNT_ID}
 PREFIX=sample-data
 
-echo "BUCKET_BUILD=${BUCKET_BUILD}"
-echo "Create S3 Bucket: ${BUCKET_BUILD} if not exist"
-aws  s3 mb s3://${BUCKET_BUILD}  >/dev/null 2>&1
+echo "BUCKET=${BUCKET}"
 
-echo "########################################################"
-echo "aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/"
-echo "########################################################"
+aws  s3 cp item_new.csv s3://${BUCKET}/${PREFIX}/ingest-data/item/
 
-cp item_new.csv ./ingest-data/item/
-
-aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/
 if [[ $? -ne 0 ]]; then
-      echo "error!!! aws  s3 sync . s3://${BUCKET_BUILD}/${PREFIX}/"
+      echo "error!!! aws s3 cp item_new.csv s3://${BUCKET}/${PREFIX}/ingest-data/item/"
       exit 1
 fi
-rm ./ingest-data/item/item_new.csv
+
 
 
