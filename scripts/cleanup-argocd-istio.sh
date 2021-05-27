b2fd4a526b49c02df57f9ceee8695b6f372fcd22
dengmingtong <dengmingtong@hotmail.com>
2021-05-27 14:18:32 +0800

    refactor cleanup argocd and istio

--- scripts/cleanup-argocd-istio.sh
+++ scripts/cleanup-argocd-istio.sh
@@ -10,32 +10,32 @@ set -e
 
 # echo 'check related resources have been cleaned up'
 
-i=0
+echo "start check istio ingress gateway security group"
+i=1
 while [ $i -le 3 ]
 do
   ISTIO_SG_ID=$(aws ec2 describe-security-groups --filter Name=tag:kubernetes.io/cluster/gcr-rs-workshop-cluster,Values=owned Name=description,Values=*istio-system/istio-ingressgateway* --query "SecurityGroups[*].[GroupId]" --output text)
-  if [ "$ISTIO_SG_ID" == "" ];
-  	echo "delete istio security group successfully!"
+  if [ "$ISTIO_SG_ID" == "" ];then
+    echo "delete istio security group successfully!"
     break
   sleep 3
+  else
+  	    echo "You are not allowed!"
+  fi
   let i++
 done
 
+echo "start check argocd server security group"
 j=0
 while [ $j -le 3 ]
 do
   ARGOCD_SG_ID=$(aws ec2 describe-security-groups --filter Name=tag:kubernetes.io/cluster/gcr-rs-workshop-cluster,Values=owned Name=description,Values=*argocd/argocd-server* --query "SecurityGroups[*].[GroupId]" --output text)
-  if [ "$ARGOCD_SG_ID" == "" ];
+  if [ "$ARGOCD_SG_ID" == "" ];then
   	echo "delete argocd security group successfully!"
-    break
+    break    
   sleep 3    
   let j++
 done
 
 echo $ISTIO_SG_ID
 echo $ARGOCD_SG_ID
-
-# ISTIO_SG_ID=$(aws ec2 describe-security-groups --filter Name=tag:kubernetes.io/cluster/gcr-rs-workshop-cluster,Values=owned Name=description,Values=*istio-system/istio-ingressgateway* --query "SecurityGroups[*].[GroupId]" --output text)
-
-# aws ec2 describe-security-groups --filter Name=tag:kubernetes.io/cluster/gcr-rs-workshop-cluster,Values=owned Name=description,Values=*argocd/argocd-server*
-
