d284632e7e6940270cf66cc3e55364e6a46f9279
dengmingtong <dengmingtong@hotmail.com>
2021-06-11 23:10:11 +0800

    clean online resources

--- scripts/cleanup-argocd-istio.sh
+++ scripts/cleanup-argocd-istio.sh
@@ -13,46 +13,42 @@ sleep 10
 echo "start check istio ingress gateway security group"
 i=1
 ISTIO_SG_ID=""
-while [ $i -le 3 ]
-do
+while true; do
   ISTIO_SG_ID=$(aws ec2 describe-security-groups --filter Name=tag:kubernetes.io/cluster/gcr-rs-workshop-cluster,Values=owned Name=description,Values=*istio-system/istio-ingressgateway* --query "SecurityGroups[*].[GroupId]" --output text)
-  if [ "$ISTIO_SG_ID" == "" ];then
+  if [ "$ISTIO_SG_ID" == "" ]; then
     echo "delete istio security group successfully!"
     break
   else
-    echo "wait for istio security group deleted!"    
+    echo "wait for istio security group deleted!"
   fi
-  sleep 3
-  let i++
+  sleep 10
 done
 
 echo "start check argocd server security group"
 j=1
 ARGOCD_SG_ID=""
-while [ $j -le 3 ]
-do
+while true; do
   ARGOCD_SG_ID=$(aws ec2 describe-security-groups --filter Name=tag:kubernetes.io/cluster/gcr-rs-workshop-cluster,Values=owned Name=description,Values=*argocd/argocd-server* --query "SecurityGroups[*].[GroupId]" --output text)
-  if [ "$ARGOCD_SG_ID" == "" ];then
+  if [ "$ARGOCD_SG_ID" == "" ]; then
     echo "delete argocd security group successfully!"
     break
   else
     echo "wait for argocd server security group deleted!"
   fi
-  sleep 3
-  let j++
+  sleep 10
 done
 
 echo $ISTIO_SG_ID
 echo $ARGOCD_SG_ID
 
-if [ "$ISTIO_SG_ID" != "" ];then
+if [ "$ISTIO_SG_ID" != "" ]; then
   echo "delete istio security group!"
   aws ec2 delete-security-group --group-id $ISTIO_SG_ID
 fi
 
-if [ "$ARGOCD_SG_ID" != "" ];then
+if [ "$ARGOCD_SG_ID" != "" ]; then
   echo "delete argocd security group!"
   aws ec2 delete-security-group --group-id $ARGOCD_SG_ID
 fi
 
-../scripts
+cd ../scripts
